c95741f1a8076e54048c56117b16297276d4c60f
Gabriel Peal <gpeal@u.northwestern.edu>
2013-10-13 18:33:56 -0700

    Made the index page work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require_tree ./libs
+//= require_tree ./plugins
+//= require_tree ./charts
+//= require_tree ./demo
 //= require_tree .
